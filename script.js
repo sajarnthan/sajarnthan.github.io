547a6e57ee110a34f5c7373de9aa91d2c93e03bd
sajarnthan <98745440+sajarnthan@users.noreply.github.com>
2022-07-07 14:16:15 +0200

    version responsive

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $(".navbar").hide();
+
+    $(function() {
+        $(window).scroll(function () {
+
+            if ($(this).scrollTop() > 140) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+
+});
